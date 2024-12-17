9b8369cd0512f80cf71385bc7ef8bcaba6916fa2
Firefly <854399598@qq.com>
2024-12-17 11:45:43 +0800

    Site updated: 2024-12-17 11:45:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/16/为 Hexo 绑定自己的域名/","2024/12/14/使用 Hexo + GitHub Pages 部署个人博客/"];function toRandomPost(){
+var posts=["2024/12/14/使用 Hexo + GitHub Pages 部署个人博客/","2024/12/16/为 Hexo 绑定自己的域名/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
